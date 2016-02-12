9e6d9cda003c0cdffd0db8fc2ce03b9d262c4378
ivelasco <ivelasco@bindeo.com>
2016-02-12 11:08:14 +0100

    Initialize

---
+++ README.md
@@ -0,0 +1,4 @@
+front
+=====
+
+A Symfony project created on February 8, 2016, 11:29 am.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     * @Route("/hello", name="hello")
+     */
+    public function testAction(Request $request)
+    {
+        return $this->render('default/hello.html.twig');
+    }
+}
