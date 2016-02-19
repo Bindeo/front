156e85c29e7f7121d06b46f9d0d6a5258a819f09
ivelasco <ivelasco@bindeo.com>
2016-02-19 09:58:05 +0100

    locale solved

--- src/AppBundle/Listener/LocaleListener.php
+++ src/AppBundle/Listener/LocaleListener.php
@@ -18,7 +18,15 @@ class LocaleListener implements EventSubscriberInterface
     public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
+
         if (!$request->hasPreviousSession()) {
+            // For a fresh start we look for the locale value in a cookie
+            if ($request->cookies->has('LOCALE')) {
+                $locale = $request->cookies->get('LOCALE');
+                $request->getSession()->set('_locale', $locale);
+                $request->setLocale($locale);
+            }
+
             return;
         }
 
@@ -26,8 +34,17 @@ class LocaleListener implements EventSubscriberInterface
         if ($locale = $request->attributes->get('_locale')) {
             $request->getSession()->set('_locale', $locale);
         } else {
-            // If no explicit locale has been set on this request, use one from the session
-            $request->setLocale($request->getSession()->get('_locale', $this->_defaultLocale));
+            if ($request->getSession()->has('_locale')) {
+                $locale = $request->getSession()->get('_locale');
+            } elseif ($request->cookies->has('LOCALE')) {
+                $locale = $request->cookies->get('LOCALE');
+                $request->getSession()->set('_locale', $locale);
+            } else {
+                $locale = $this->_defaultLocale;
+            }
+
+            // If no explicit locale has been set on this request, use one from the session or the cookie
+            $request->setLocale($locale);
         }
     }
 
--- src/AppBundle/Security/AuthenticationSuccess.php
+++ src/AppBundle/Security/AuthenticationSuccess.php
@@ -4,6 +4,7 @@ namespace AppBundle\Security;
 
 use AppBundle\Model\ApiConnection;
 use \Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,8 +24,7 @@ class AuthenticationSuccess implements AuthenticationSuccessHandlerInterface
 
     /**
      * This is called when an interactive authentication attempt succeeds. This
-     * is called by authentication listeners inheriting from
-     * AbstractAuthenticationListener.
+     * is called by authentication listeners inheriting from AbstractAuthenticationListener.
      *
      * @param Request        $request
      * @param TokenInterface $token
@@ -34,17 +34,31 @@ class AuthenticationSuccess implements AuthenticationSuccessHandlerInterface
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
         // The user has logged into the system, we tell it to the API
-        $res = $this->_api->getJson($this->_api->getRoute('account'), ['email' => $token->getUsername(), 'password' => $request->get('_password'), 'ip' => $request->getClientIp()]);
+        $res = $this->_api->getJson($this->_api->getRoute('account'), [
+            'email'    => $token->getUsername(),
+            'password' => $request->get('_password'),
+            'ip'       => $request->getClientIp()
+        ]);
         if ($res->getError() or !($user = $res->getRows()[0])) {
             // There is a problem login in the api, we need to logout the user
-            $this->_monolog->critical('CRITICAL ERROR: Login into API', ['email' => $token->getUsername(), 'password' => $request->get('_password'), 'ip' => $request->getClientIp()]);
+            $this->_monolog->critical('CRITICAL ERROR: Login into API', [
+                'email'    => $token->getUsername(),
+                'password' => $request->get('_password'),
+                'ip'       => $request->getClientIp()
+            ]);
+
             return new RedirectResponse('/logout');
         }
 
         // if the user hit a secure page and start() was called, this was
         // the URL they were on, and probably where you want to redirect to
-        $targetPath = $request->getSession()->get('_security.'.$token->getProviderKey().'.target_path');
+        $targetPath = $request->getSession()->get('_security.' . $token->getProviderKey() . '.target_path');
 
-        return new RedirectResponse($targetPath ? $targetPath : '/');
+        $response = new RedirectResponse($targetPath ? $targetPath : '/');
+
+        // Save the user language in a cookie
+        $response->headers->setCookie(new Cookie('LOCALE', $token->getUser()->getLang(), 31536000 + time()));
+
+        return $response;
     }
 }
