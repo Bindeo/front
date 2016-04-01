f564b345aa5067dc8f0b450aa59ab5402989354d
ivelasco <ivelasco@bindeo.com>
2016-04-01 14:40:38 +0200

    Public API resources

--- src/AppBundle/Entity/ResultSet.php
+++ src/AppBundle/Entity/ResultSet.php
@@ -32,7 +32,7 @@ class ResultSet extends ClientResultSetAbstract
                 $object = new User((array)$data->attributes);
                 $res = [$object->getIdUser(), $object];
                 break;
-            case 'user_identity':
+            case 'user_identities':
                 // Fill the entity class
                 if (!$this->entity) {
                     $this->entity = 'AppBundle\Entity\UserIdentity';
