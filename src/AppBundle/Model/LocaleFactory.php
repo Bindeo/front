89e0f1a56d0111ed9b10f9744841b2417421d3b9
ivelasco <ivelasco@bindeo.com>
2016-05-11 18:23:01 +0200

    #33 Signing by mobile code

--- src/AppBundle/Model/LocaleFactory.php
+++ src/AppBundle/Model/LocaleFactory.php
@@ -7,20 +7,33 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class LocaleFactory
 {
     private $number;
-    private $date;
 
     public function __construct(Session $session)
     {
         $this->number = new \NumberFormatter($session->get('_locale'), \NumberFormatter::DECIMAL);
     }
 
+    /**
+     * Format number into locale
+     *
+     * @param string $number
+     *
+     * @return string
+     */
     public function format($number)
     {
         return $this->number->format($number);
     }
 
+    /**
+     * Format currency
+     *
+     * @param string $amount
+     *
+     * @return string
+     */
     public function formatCurrency($amount)
     {
-        return $this->number->formatCurrency();
+        return $this->number->formatCurrency($amount);
     }
 }
